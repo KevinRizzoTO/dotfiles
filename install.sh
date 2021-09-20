1e3defb91f26e4bf20ef038b4e239eb25480d019
KevinRizzoTO <contact@kevinrizzo.com>
2021-09-20 15:34:18 -0400

    Changing path for install

--- install.sh
+++ install.sh
@@ -1,29 +1,58 @@
-PACKAGES=(
-	stow
-	alfred
-	iterm2
-	insomnia
-	rectangle
-	fzf
-	bat
-	rg
-	yarn
-	tmuxinator
-	git-delta
-	lazygit
-	kitty
-	font-clear-sans
-	font-consolas-for-powerline
-	font-dejavu-sans-mono-for-powerline
-	font-fira-code
-	font-fira-mono-for-powerline
-	font-inconsolata
-	font-inconsolata-for-powerline
-	font-liberation-mono-for-powerline
-	font-menlo-for-powerline
-	font-roboto
-)
-brew install "${PACKAGES[@]}"
+# install Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew all the things
+
+brew install stow
+
+brew install alfred
+
+brew install insomnia
+
+brew install rectangle
+
+brew install fzf
+
+brew install bat
+
+brew install rg
+
+brew install yarn
+
+brew install tmuxinator
+
+brew install git-delta
+
+brew install lazygit
+
+brew install kitty
+
+brew install font-clear-sans
+
+brew install font-consolas-for-powerline
+
+brew install font-dejavu-sans-mono-for-powerline
+
+brew install font-fira-code
+
+brew install font-fira-mono-for-powerline
+
+brew install font-inconsolata
+
+brew install font-inconsolata-for-powerline
+
+brew install font-liberation-mono-for-powerline
+
+brew install font-menlo-for-powerline
+
+brew install font-roboto
+
+brew install visual-studio-code
+
+brew install --HEAD neovim
+
+brew install direnv
 
 # oh my zsh
 
@@ -37,6 +66,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# neovim (at HEAD)
+# oh my zsh plugins
 
-brew install --HEAD neovim
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+git clone https://github.com/softmoth/zsh-vim-mode $ZSH_CUSTOM/plugins/zsh-vim-mode
