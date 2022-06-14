b2c18ba55a97542625b825fab24c968e22040cf9
Kevin Rizzo <kevin.rizzo@shopify.com>
2022-06-14 16:04:46 -0400

    Copilot changes, setup needs to be done everytime

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -225,6 +225,15 @@ require('dapui').setup()
   end
 })
 
+-- copilot.vim
+
+vim.cmd([[
+
+imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+let g:copilot_no_tab_map = v:true
+
+]])
+
 -- lualine
 
 require('lualine').setup({
